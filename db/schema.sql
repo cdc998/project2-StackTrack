417a0ec52ed9371894404cce2c276794921c9e87
jinki <jin.kikuchi98@gmail.com>
2024-10-02 15:20:18 +1000

    finished CRUD for MVP

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE stacktrack;
 
 CREATE TABLE play_history (
-    play_id SERIAL PRIMARY KEY,
+    hand_id SERIAL PRIMARY KEY,
     user_id INTEGER,
     blind_level VARCHAR,
     session_date DATE,
@@ -16,27 +16,15 @@ CREATE TABLE users (
     password_digest TEXT
 );
 
-INSERT INTO play_history (user_id, blind_id, session_date, win_loss_amount, session_duration, notes)
+INSERT INTO play_history (user_id, blind_level, session_date, win_loss, notes)
 VALUES
-(1, '2/5', '2024-09-25', 200.00, '02:30:00', 'Solid session, ran well with pocket Aces.'),
-(1, '2/3', '2024-09-26', -150.00, '01:45:00', 'Lost a big hand on the river.'),
-(1, '5/10', '2024-09-27', 50.00, '03:15:00', 'Slow session but ended slightly up.'),
-(1, '2/5', '2024-09-28', 400.00, '04:00:00', 'Hit a flush on the turn for a big pot.'),
-(1, '2/5', '2024-09-29', -100.00, '02:00:00', 'Got bluffed off a hand but had a few good wins.');
+(1, '2/5', '2024-09-25', 200.00, 'Solid session, ran well with pocket Aces.'),
+(1, '2/3', '2024-09-26', -150.00, 'Lost a big hand on the river.'),
+(1, '5/10', '2024-09-27', 50.00, 'Slow session but ended slightly up.'),
+(1, '2/5', '2024-09-28', 400.00, 'Hit a flush on the turn for a big pot.'),
+(1, '2/5', '2024-09-29', -100.00, 'Got bluffed off a hand but had a few good wins.');
 
 
-
-INSERT INTO dishes (title, image_url, description) VALUES ('cake', 'https://www.recipetineats.com/tachyon/2020/08/My-best-Vanilla-Cake_9-SQ.jpg', 'vanilla cake');
-
-INSERT INTO dishes (title, image_url, description) VALUES ('bubble tea', 'https://upload.wikimedia.org/wikipedia/commons/a/a2/Bubble_Tea.png', 'bubble tea');
-
-INSERT INTO dishes (title, image_url, description) VALUES ('skewers', 'https://www.kitchensanctuary.com/wp-content/uploads/2016/04/Honey-Garlic-Chicken-Skewers-square-FS-21.jpg', 'honey garlic chicken skewers');
-
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email TEXT UNIQUE,
-    password_digest TEXT
-);
 
 ALTER TABLE dishes ADD COLUMN user_id INTEGER;
 
--- server.js
+++ server.js
@@ -35,8 +35,107 @@ app.get('/', (req, res) => {
 })
 
 app.get('/playhistory', (req, res) => {
-    res.render('play_history')
+    const sql = `
+    SELECT * FROM 
+    play_history WHERE 
+    user_id = $1
+    ORDER BY 
+    session_date DESC;
+    `
+
+    db.query(sql, [req.session.userId], (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+
+        console.log(result)
+
+        let handsHistory = result.rows
+
+        res.render('play_history', {handsHistory})
+    })
 }) 
+
+app.get('/playhistory/:handId/edit', (req, res) => {
+    const sql = `
+    SELECT * FROM play_history 
+    WHERE hand_id = $1;
+    `
+
+    db.query(sql, [req.params.handId], (err, result) => {
+        const hand = result.rows[0]
+        res.render('play_edit_form', {hand})
+    })
+})
+
+app.get('/playhistory/new', (req, res) => {
+    res.render('new_play_form')
+})
+
+app.post('/playhistory', (req, res) => {
+    const user_id = req.session.userId
+    const blind_level = req.body.blind_level
+    const session_date = req.body.session_date
+    const win_loss = req.body.win_loss
+    const notes = req.body.notes
+
+    const sql = `
+    INSERT INTO play_history 
+    (user_id, blind_level, session_date, win_loss, notes) 
+    VALUES 
+    ($1, $2, $3, $4, $5)
+    `
+
+    db.query(sql, [user_id, blind_level, session_date, win_loss, notes], (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+
+        res.redirect('/playhistory')
+    })
+})
+
+app.put('/playhistory/:handId', (req, res) => {
+    const blind_level = req.body.blind_level
+    const session_date = req.body.session_date
+    const win_loss = req.body.win_loss
+    const notes = req.body.notes
+    const hand_id = req.body.hand_id
+
+    const sql = `
+    UPDATE play_history 
+    SET blind_level = $1, session_date = $2, 
+    win_loss = $3, notes = $4
+    WHERE hand_id = $5;
+    `
+
+    db.query(sql, [blind_level, session_date, win_loss, notes, hand_id], (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+
+        console.log(result)
+
+        res.redirect('/playhistory')
+    })
+})
+
+app.delete('/playhistory/:handId', (req, res) => {
+    const sql = `
+    DELETE FROM play_history 
+    WHERE hand_id = $1;
+    `
+
+    db.query(sql, [req.params.handId], (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        res.redirect('/playhistory')
+    })
+})
+
+
+
 
 
 
@@ -75,6 +174,7 @@ app.post('/login', (req, res) => {
             }
 
             req.session.userId = user.user_id
+
             res.redirect('/playhistory')
         })
     })
@@ -106,10 +206,8 @@ app.post('/signup', (req, res) => {
                 if (err) {
                     console.log(err)
                 }
-
-                const user = result.rows[0]
                 
-                res.redirect('/', {user})
+                res.redirect('/')
             })
         })
     })
